58adbbd0c23a1a6b1aa4d04303c1ac0cc1081a51
Ava Schmidt <ava.126s@gmail.com>
2026-05-22 18:45:23 -0400

    change layout

---
+++ static/js/map.js
@@ -0,0 +1,23 @@
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiYXZhamlhbiIsImEiOiJjbXBoaWJrcXgxMGtrMnlweXFhaHp5dTVvIn0.CHSelWQsEWlEbJqy7MnRiw";
+const previewMap = new mapboxgl.Map({
+  container: "map-preview",
+  style: "mapbox://styles/mapbox/light-v11",
+  projection: "globe",
+  zoom: 1,
+  center: [0, 20],
+  interactive: false, // no zoom/pan in preview
+});
+
+previewMap.on("style.load", () => {
+  previewMap.setFog({}); // globe atmosphere
+});
+
+// Spin animation
+let spinAngle = 0;
+function spinGlobe() {
+  spinAngle += 0.04; // rotation speed
+  previewMap.setCenter([spinAngle, 20]);
+  requestAnimationFrame(spinGlobe);
+}
+spinGlobe();
---
+++ static/js/script.js
@@ -0,0 +1,4 @@
+// click name animation when page loads
+window.onload = function () {
+  document.getElementById("sparkle-name").click();
+};
