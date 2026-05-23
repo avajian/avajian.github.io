fc1385f6e60014111a617668999af45d8fd69511
Ava <avajian@umich.edu>
2026-05-23 00:39:32 -0400

    dark mode and styles

---
+++ static/js/dark-mode.js
@@ -0,0 +1,22 @@
+document
+  .querySelector(".sparkle a")
+  .addEventListener("click", function (event) {
+    // Prevents the page from reloading or navigating if it's an empty link
+    event.preventDefault();
+
+    document.body.classList.toggle("dark-mode");
+  });
+
+document
+  .querySelector("#dark-mode")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+      this.textContent = "★ Light mode";
+    } else {
+      this.textContent = "★ Dark mode";
+    }
+  });
